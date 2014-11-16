1856491691326a36f98572534fa73615b709b3b1
Ezra Rufino <ezrarufino@nymb.co>
2014-11-15 18:26:53 -0800

    Fully implemented devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'ezra-rules.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
